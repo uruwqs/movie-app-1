cc2cc29f233604112284bae9f2054721bb88680a
Yerassyl Auyeskhan <95128018+ieraasyl@users.noreply.github.com>
2026-07-13 20:44:10 +0500

    feat: get movies by id

--- server/main.py
+++ server/main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 
 from data import movies
 from schemas import Movie
 
 app = FastAPI(title="Movie API")
+
+
+def find_movie_index(movie_id: int) -> int:
+    for index, movie in enumerate(movies):
+        if movie.id == movie_id:
+            return index
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Movie not found",
+    )
 
 
 @app.get("/")
@@ -14,3 +25,9 @@ def home():
 @app.get("/api/movies", response_model=list[Movie])
 def get_movies():
     return movies
+
+
+@app.get("/api/movies/{movie_id}", response_model=Movie)
+def get_movie(movie_id: int):
+    movie_index = find_movie_index(movie_id)
+    return movies[movie_index]
